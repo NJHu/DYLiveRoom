aeb0715b5383807809354009eaee1fcf1d9b7856
胡鹏 <64hp@163.com>
2018-07-21 14:56:24 +0800

    支持屏幕旋转

--- DYLiveRoom/Classes/Controller/NJDYLiveRoomController.swift
+++ DYLiveRoom/Classes/Controller/NJDYLiveRoomController.swift
@@ -9,19 +9,16 @@ import UIKit
 import NJKit
 import NJDYPlayer
 
+fileprivate let WHScale: CGFloat =  0.56;
+
 public class NJDYLiveRoomController: NJViewController {
     public var roomId: String?
     private var liveUrl: String?
-    private let containerView = UIView()
-    private var maskControlView = UIView()
-    private var moviePlayer: NJPlayerController?
+    private let containerView = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * WHScale))
+    private lazy var moviePlayer: NJPlayerController = NJPlayerController(containerView: self.containerView, delegate: self)
     override public func viewDidLoad() {
         super.viewDidLoad()
-        nj_interactivePopDisabled = true
-        title = "斗鱼直播间"
-        view.backgroundColor = UIColor.groupTableViewBackground
-        setupPlayer()
-        setupMaskControlView()
+        setupLiveRoomUI()
         
         if let roomId = self.roomId {
             NJProgressHUD.showLoading(in: self.view)
@@ -30,8 +27,8 @@ public class NJDYLiveRoomController: NJViewController {
             // 获得直播流
             NJLiveRoomStreamTool.sharedTool.nj_getStreamUrl(roomH5Url: roomUrl, elementId: elementId, success: {[weak self] (roomUrl, streamUrl) in
                 self?.liveUrl = streamUrl
-                if self?.moviePlayer?.isPlaying != nil && !(self!.moviePlayer!.isPlaying) {
-                    self?.moviePlayer?.prepareToPlay(contentURLString: streamUrl)
+                if self?.moviePlayer.isPlaying != nil && !(self!.moviePlayer.isPlaying) {
+                    self?.moviePlayer.prepareToPlay(contentURLString: streamUrl)
                 }
                 NJProgressHUD.hideLoading(in: self?.view)
             }) {[weak self] (roomUrl, error) in
@@ -40,37 +37,53 @@ public class NJDYLiveRoomController: NJViewController {
             }
         }
     }
-    public override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        if let liveUrl = self.liveUrl {
-            moviePlayer?.prepareToPlay(contentURLString: liveUrl)
+}
+
+// MARK:- UI
+extension NJDYLiveRoomController {
+    private func setupLiveRoomUI() {
+        let statusBarBg = UIView()
+        statusBarBg.backgroundColor = UIColor.black
+        view.addSubview(statusBarBg)
+        statusBarBg.snp.makeConstraints { (make) in
+            make.left.right.top.equalToSuperview()
+            make.height.equalTo(50)
         }
-    }
-    public override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        moviePlayer?.shutdown()
+        nj_navigationBar.isHidden = true
+        title = "斗鱼直播间"
+        view.backgroundColor = UIColor.groupTableViewBackground
+        view.addSubview(containerView)
+        containerView.backgroundColor = UIColor.black
     }
     
     public override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
+        if nj_interfaceOrientation_isPortrait {
+            containerView.frame = CGRect(x: 0, y: UIApplication.shared.statusBarFrame.height, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * WHScale)
+        }else {
+            containerView.frame = CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height)
+        }
     }
 }
-// MARK:- UI
+
+// MARK:- view-life
 extension NJDYLiveRoomController {
-    private func setupPlayer() {
-        
-        view.addSubview(containerView)
-        containerView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        containerView.frame = CGRect(x: 0, y: nj_navigationBar.frame.maxY, width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.width * 0.56)
-        maskControlView.backgroundColor = UIColor.clear
-        
-        moviePlayer = NJPlayerController(containerView: containerView, delegate: self)
+    public override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        if let liveUrl = self.liveUrl, !moviePlayer.isPlaying {
+            moviePlayer.prepareToPlay(contentURLString: liveUrl)
+        }
     }
-    private func setupMaskControlView() -> Void {
-        containerView.addSubview(maskControlView)
-        maskControlView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
-        maskControlView.frame = containerView.bounds
-        maskControlView.backgroundColor = UIColor.clear
+    public override func viewDidAppear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    public override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+    }
+    public override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        moviePlayer.shutdown()
     }
 }
 
@@ -97,13 +110,13 @@ extension NJDYLiveRoomController {
         return false
     }
     public  override var preferredStatusBarStyle: UIStatusBarStyle {
-        return UIStatusBarStyle.default
+        return UIStatusBarStyle.lightContent
     }
     public  override var preferredStatusBarUpdateAnimation: UIStatusBarAnimation {
         return UIStatusBarAnimation.slide
     }
     public override var shouldAutorotate: Bool {
-        return false
+        return true
     }
     // MARK: - about keyboard orientation
     public override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
@@ -117,5 +130,5 @@ extension NJDYLiveRoomController {
 
 // MARK:- action
 extension NJDYLiveRoomController {
-
+    
 }
